c71917e2077d93231cfdbb8df4167cb74082b2ed
Chris Cali <ChristopherCali1415@gmail.com>
2020-11-22 22:49:08 -0500

    Implement breakCamelCase

---
+++ breakCamelCase.js
@@ -0,0 +1,20 @@
+function solution(string) {
+  var resultArr = [];
+
+  string.split('').forEach((char) => {
+    if (switcher(char)) {
+      resultArr.push(' ');
+    }
+    resultArr.push(char);
+  });
+
+  var resultStr = resultArr.join('');
+  return resultStr;
+}
+
+function switcher (char) {
+  if (char !== char.toLowerCase()) {
+    return true;
+  }
+  return false;
+}
